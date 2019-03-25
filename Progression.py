05a4311898927358290f580c529ac92691599d36
developeroka <developeroka@gmail.com>
2019-03-25 20:01:17 +0430

    inherit Fibonacci Progression from progression super class

--- Progression.py
+++ Progression.py
@@ -47,3 +47,15 @@ class GeometricProgression(Progression):
 
     def _advanced(self):
         self._current *= self._base
+
+
+class FibonacciProgression(Progression):
+
+    def __init__(self, first=0, second=1):
+
+        super().__init__(first)
+        self._prev = second - first
+
+    def _advanced(self):
+        self._prev, self._current = self._current, self._prev + self._current
+

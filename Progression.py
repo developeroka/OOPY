1ef80d7a9af0422f13044f893fc583ce6ae0cdc4
developeroka <developeroka@gmail.com>
2019-03-25 19:47:53 +0430

    inherit Geometric Progression from progression super class

--- Progression.py
+++ Progression.py
@@ -37,3 +37,13 @@ class ArithmeticProgression(Progression):
     def _advance(self):
 
         self._current += self._increment
+
+
+class GeometricProgression(Progression):
+
+    def __init__(self, base=2, start=1):
+        super().__init__(start)
+        self._base = base
+
+    def _advanced(self):
+        self._current *= self._base

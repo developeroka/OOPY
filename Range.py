788846befc0ee5d11e705fd5ae8c93516c70b5bf
developeroka <developeroka@gmail.com>
2019-03-25 14:06:42 +0430

    implement len() and getitem() special methods

--- Range.py
+++ Range.py
@@ -14,8 +14,22 @@ class Range:
         if stop is None:
             start, stop = 0, start
 
-        # ‌ calculate the length
         self._length = max(0, (stop - start + step - 1) // step)
-
         self._start = start
-        self._stop = stop
+        self._step = step
+
+    def __len__(self):
+
+        return self._length
+
+    def __getitem__(self, item):
+
+        if item < 0:
+            item += len(self)
+
+        if not 0 <= item < self._length:
+            raise IndexError('index out of range')
+
+        return self._start + item * self._step
+
+

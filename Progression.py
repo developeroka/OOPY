50b3db09873443d4207054fbc071a3bb9484bfb7
developeroka <developeroka@gmail.com>
2019-03-25 21:18:19 +0430

    testing Progression and its subclasses

--- Progression.py
+++ Progression.py
@@ -30,12 +30,10 @@ class Progression:
 class ArithmeticProgression(Progression):
 
     def __init__(self, increment=1, start=0):
-
         super().__init__(start)
         self._increment = increment
 
-    def _advance(self):
-
+    def _advanced(self):
         self._current += self._increment
 
 
@@ -58,4 +56,21 @@ class FibonacciProgression(Progression):
 
     def _advanced(self):
         self._prev, self._current = self._current, self._prev + self._current
+
+
+if __name__ == '__main__':
+    print('Default progression:')
+    Progression().print_progression(10)
+    print('Arithmetic progression with increment 5:')
+    ArithmeticProgression(5).print_progression(10)
+    print('Arithmetic progression with increment 5 and start 2:')
+    ArithmeticProgression(5, 2).print_progression(10)
+    print('Geometric progression with default base:')
+    GeometricProgression().print_progression(10)
+    print('Geometric progression with base 3:')
+    GeometricProgression(3).print_progression(10)
+    print('Fibonacci progression with default start values:')
+    FibonacciProgression().print_progression(10)
+    print('Fibonacci progression with start values 4 and 6:')
+    FibonacciProgression(4, 6).print_progression(10)
 

e7a3eb9fc649d5131e426f6c5e722e8d80d8bbfb
developeroka <developeroka@gmail.com>
2019-03-25 18:01:48 +0430

    write CreditCard functions for interacting with account

--- CredirCard.py
+++ CredirCard.py
@@ -6,3 +6,34 @@ class CreditCard:
         self._account = account
         self._limit = limit
         self._balance = 0
+
+    def get_customer(self):
+        return self._customer
+
+    def get_bank(self):
+        return self._bank
+
+    def get_account(self):
+        return self._account
+
+    def get_limit(self):
+        return self._limit
+
+    def get_balance(self):
+        return self._balance
+
+    def make_payment(self, amount):
+
+        if self._balance - amount < 0:
+            raise ValueError('not enough balance')
+
+        self._balance -= amount
+
+    def charge(self, price):
+
+        if self._balance + price > self._limit:
+            return False
+
+        else:
+            self._balance += price
+            return True

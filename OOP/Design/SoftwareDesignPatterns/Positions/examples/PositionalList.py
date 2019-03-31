9e1371a04d49b1c6f7f79ee5b2d9eccbd74ad436
developeroka <developeroka@gmail.com>
2019-03-31 18:50:54 +0430

    implement nested position class

--- OOP/Design/SoftwareDesignPatterns/Positions/examples/PositionalList.py
+++ OOP/Design/SoftwareDesignPatterns/Positions/examples/PositionalList.py
@@ -1,4 +1,3 @@
-
 class _DoublyLinkedBase:
     """A base class providing a doubly linked list representation."""
 
@@ -47,4 +46,28 @@ class _DoublyLinkedBase:
         return element                                  # return deleted element
 
 
+class PositionalList(_DoublyLinkedBase):
+    """A sequential container of elements allowing positional access."""
+
+    # -------------------------- nested Position class --------------------------
+
+    class Position:
+        """An abstraction representing the location of a single element."""
+
+        def __init__(self, container, node):
+            """Constructor should not be invoked by user."""
+            self._container = container
+            self._node = node
+
+        def element(self):
+            """Return the element stored at this Position."""
+            return self._node._element
+
+        def __eq__(self, other):
+            """Return True if other is a Position representing the same location."""
+            return type(other) is type(self) and other._node is self._node
+
+        def __ne__(self, other):
+            """Return True if other does not represent the same location."""
+            return not (self == other)                       # opposite of eq
 

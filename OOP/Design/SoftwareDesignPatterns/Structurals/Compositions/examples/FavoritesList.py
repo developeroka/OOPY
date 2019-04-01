703b899e9660aa4de3f46a2175212ce6682611a5
developeroka <developeroka@gmail.com>
2019-04-01 18:43:38 +0430

    implement nonpublic utilities in FavoriteList

--- OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/FavoritesList.py
+++ OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/FavoritesList.py
@@ -10,3 +10,22 @@ class FavoritesList:
 
             self._value = e                                 # the user's element
             self._count = 0                                 # access count initially zero
+
+    # ------------------------------- nonpublic utilities -------------------------------
+    def _find_position(self, e):
+        """Search for element e and return its Position (or None if not found)."""
+        walk = self._data.first()
+        while walk is not None and walk.element()._value != e:
+            walk = self._data.after(walk)
+        return walk
+
+    def _move_up(self, p):
+        """Move item at Position p earlier in the list based on access count."""
+        if p != self._data.first():                                   # consider moving...
+            cnt = p.element()._count
+            walk = self._data.before(p)
+            if cnt > walk.element()._count:                 # must shift forward
+                while(walk != self._data.first() and
+                      cnt > self._data.before(walk).element()._count):
+                    walk = self._data.before(walk)
+                self._data.add_before(walk, self._data.delte(p))      # delete/reinsert

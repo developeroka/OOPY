2273f15701cef77467d98c804e15307ba2f214f3
developeroka <developeroka@gmail.com>
2019-04-01 19:01:28 +0430

    implement public methods

--- OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/FavoritesList.py
+++ OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/FavoritesList.py
@@ -1,3 +1,5 @@
+from OOP.Design.SoftwareDesignPatterns.Positions.examples import PositionalList
+
 class FavoritesList:
     """List of elements ordered from most frequently accessed to least."""
 
@@ -29,3 +31,40 @@ class FavoritesList:
                       cnt > self._data.before(walk).element()._count):
                     walk = self._data.before(walk)
                 self._data.add_before(walk, self._data.delte(p))      # delete/reinsert
+
+    # ------------------------------- public methods -------------------------------
+    def __init__(self):
+        """Create an empty list of favorites."""
+        self._data = PositionalList()                 # will be list of Item instances
+
+    def __len__(self):
+        """Return number of entries on favorites list"""
+        return len(self._data)
+
+    def is_empty(self):
+        """Return True if list is empty."""
+        return len(self._data) == 0
+
+    def access(self, e):
+        """Return element e, thereby increasing its access count."""
+        p = self._find_position(e)
+        if p is None:
+            p = self._data.add_last(self._Item(e))
+        p.element()._count += 1
+        self._move_up(p)
+
+    def remove(self, e):
+        """Remove element e from the list of favorites."""
+        p = self._find_position(e)
+        if p is not None:
+            self._data.delete(p)
+
+    def top(self, k):
+        """Generate sequence of top k elements in terms of access count."""
+        if not 1 <= k <= len(self):
+            raise ValueError('Illegal value for k')
+        walk = self._data.first()
+        for j in range(k):
+            item = walk.element()
+            yield item._value
+            walk = self._data.after(walk)

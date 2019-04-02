bf38ff5fc1d5e7e2195d9a608648c8b0dbfb1e8e
developeroka <developeroka@gmail.com>
2019-04-03 00:11:08 +0430

    implement is_empty method in priority queue base class

--- OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/PriorityQueueBase.py
+++ OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/PriorityQueueBase.py
@@ -2,7 +2,15 @@ class PriorityQueueBase:
     """Abstract base class for a priority queue."""
     class Item:
         """Lightweight composite to store priority queue items."""
-        slots = '_key ', '_value'
-        def __init__ (self, k, v):
-            self. key = k
-            self. value = v
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v
+
+        def __lt__(self, other):
+            return self._key < other. key       # compare items based on their keys
+
+    def is_empty(self):                         # concrete method assuming abstract len
+        """Return True if the priority queue is empty."""
+        return len(self) == 0

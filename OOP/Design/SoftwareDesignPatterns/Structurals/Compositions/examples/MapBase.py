32a309ccf5fa78a18e89c4f4278ce4123310a8ab
developeroka <developeroka@gmail.com>
2019-04-03 15:43:57 +0430

    initialize MapBase class

--- OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/MapBase.py
+++ OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/MapBase.py
@@ -3,3 +3,12 @@ from collections import MutableMapping
 
 class MapBase(MutableMapping):
     """Our own abstract base class that includes a nonpublic Item class"""
+
+    # ------------------------------- nested Item class -------------------------------
+    class _Item:
+        """”Lightweight composite to store key-value pairs as map items."""
+        __slots__ = '_key', '_value'
+
+        def __init__(self, k, v):
+            self._key = k
+            self._value = v

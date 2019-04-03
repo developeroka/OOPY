3ad1352393f2423fce346bb4fa0e41a45345192c
developeroka <developeroka@gmail.com>
2019-04-03 15:55:59 +0430

    implement nested _item class in MapBase class

--- OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/MapBase.py
+++ OOP/Design/SoftwareDesignPatterns/Structurals/Compositions/examples/MapBase.py
@@ -12,3 +12,15 @@ class MapBase(MutableMapping):
         def __init__(self, k, v):
             self._key = k
             self._value = v
+
+        def __eq__(self, other):
+
+            return self._key == other._key
+
+        def __ne__(self, other):
+
+            return not(self == other)
+
+        def __lt__(self, other):
+
+            return self._key < other._key

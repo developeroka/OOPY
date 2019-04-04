d91273a93da2978e8f99813f36ffe599d8323323
developeroka <developeroka@gmail.com>
2019-04-04 15:19:37 +0430

    implement concrete methods for Sequence class

--- OOP/Design/SoftwareDesignPatterns/Behaviorals/TemplateMethods/examples/Sequence.py
+++ OOP/Design/SoftwareDesignPatterns/Behaviorals/TemplateMethods/examples/Sequence.py
@@ -10,3 +10,25 @@ class Sequence(metaclass=ABCMeta):
     @abstractmethod
     def __getitem__(self, j):
         """Return the element at index j of the sequence."""
+
+    def __contains__(self, val):
+        """Return True if val found in the sequence; False otherwise."""
+        for j in range(len(self)):
+            if self[j] == val:                  # found match
+                return True
+        return False
+
+    def index(self, val):
+        """Return leftmost index at which val is found (or raise ValueError). """
+        for j in range(len(self)):
+            if self[j] == val:                  # leftmost match
+                return j
+            raise ValueError('value not in sequence')   # never found a match
+
+    def count(self, val):
+        """Return the number of elements equal to given value."""
+        k = 0
+        for j in range(len(self)):
+            if self[j] == val:                  # found a match
+                k += 1
+            return k
